77732c421ce00d4f339610d84faaf49331a714a8
AnnaZuzkova <famzu2018@gmail.com>
2025-01-30 20:18:25 +0200

    maybe done

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,13 @@
 import Handlebars from "handlebars";
-import template from "../handlebars/template";
-const students = [
-  {
-    name: "Anna",
-    age: 15,
-    cource: "frontend",
-  },
-  {
-    name: "Vlad",
-    age: 15,
-    cource: "frontend",
-  },
-  {
-    name: "Viacheslav",
-    age: 15,
-    cource: "frontend",
-  },
-  {
-    name: "Max",
-    age: 16,
-    cource: "frontend",
-  },
-];
+import template from "bundle-text:../templates/templates";
+import { students } from "./students";
 
-for (const student of students) {
-  const markup = template(student);
-  const container = document.querySelector("#students-container");
-  container.insertAdjacentHTML("afterend", markup);
-}
+const studentContainer = document.querySelector(".students-container");
+
+const renderStudents = (source, template) => {
+  const studentTemplate = Handlebars.compile(template);
+  const html = studentTemplate(source);
+
+  studentContainer.innerHTML = html;
+};
+renderStudents(students, template);
---
+++ src/js/students.js
@@ -0,0 +1,30 @@
+export const students = [
+  {
+    name: "Макс",
+    age: 15,
+    leave: "десь біля Київа?..",
+    cours: "frondEnd",
+    term: 4,
+  },
+  {
+    name: "В'ячеслав",
+    age: 15,
+    leave: "село Ранцево, Черкаська область",
+    cours: "frondEnd",
+    term: 4,
+  },
+  {
+    name: "Влад",
+    age: 15,
+    leave: "десь в Канаді",
+    cours: "frondEnd",
+    term: 4,
+  },
+  {
+    name: "Аня",
+    age: 15,
+    leave: "Київ",
+    cours: "frondEnd",
+    term: 4,
+  },
+];
